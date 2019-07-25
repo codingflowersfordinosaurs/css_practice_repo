a2dd86338cf6881a26fa481bb66d57ff436522b4
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-24 22:42:39 -0400

    redid the home page; media queries need to be redone; following pages need to be redone too

---
+++ js/navbar.js
@@ -0,0 +1,10 @@
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollpos = window.pageYOffset;
+  if (prevScrollpos > currentScrollpos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById('navbar').style.top = "-10em";
+  }
+  prevScrollpos = currentScrollpos;
+}
